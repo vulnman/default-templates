9a5870446ef4d9ef6f1ac13eec7b9d6955c2d97d
blockomat2100 <secret@random.com>
2022-09-07 11:54:27 +0200

    initial commit

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='vulnman_default_templates',
+    version='0.1.0',
+    packages=find_packages(),
+    url='',
+    license='',
+    author='user',
+    author_email='',
+    include_package_data=True,
+    description='Default vulnman templates'
+)
